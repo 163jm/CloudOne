16059de5c71b83778176c1dbc811b12a6f6bdb29
evecus <jamie55@163.com>
2026-05-08 11:02:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# 1. 启动 SSH 服务
-# -D 选项让 sshd 不脱离控制台（通常用于调试），但在脚本中我们通常直接启动它
-# 如果你的镜像里没有配置好 host keys，可能需要先执行 ssh-keygen
-echo "Starting SSH server..."
-/usr/sbin/sshd
-
 # 2. 如果开启了 CF 且 Token 不为空，则启动 cloudflared 隧道
 if [ "$CF" = "true" ] && [ -n "$TOKEN" ]; then
     echo "Starting cloudflared tunnel..."
